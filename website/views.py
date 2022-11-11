9a2a26a083a033963c29796d7d26c6fc4aff741e
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2022-11-11 14:57:16 -0800

    Create views.py

---
+++ website/views.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from . import mysql
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+@views.route("/welcomepage")
+def welcomePage():
+    return render_template("WelcomePage.html")
