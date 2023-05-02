74278bdf7b2b5cbc8533fae46757abaa563f8915
Luke Smith <98189885+CSLuke1718@users.noreply.github.com>
2023-05-02 15:06:29 -0700

    adding more data

--- test data.sql
+++ test data.sql
@@ -1,19 +1,109 @@
 /*Users fake data*/
+-- the hash is 1234
 INSERT INTO Users VALUES(1001, 'CSLuke', 'csluke@outlook.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Luke', 'Smith',0);
-INSERT INTO Users VALUES(1002, 'Vini', 'vini@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Vinicius', '',0);
+INSERT INTO Users VALUES(1002, 'Vini', 'vini@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Vinicious', '',0);
+INSERT INTO Users VALUES(1003, 'Alice', 'alice@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Alice', 'Smith', 5);
+INSERT INTO Users VALUES(1004, 'Bob', 'bob@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Bob', 'Johnson', 8);
+INSERT INTO Users VALUES(1005, 'Charlie', 'charlie@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Charlie', 'Brown', 2);
+INSERT INTO Users VALUES(1006, 'Dave', 'dave@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'David', 'Lee', 15);
+INSERT INTO Users VALUES(1007, 'Emily', 'emily@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Emily', 'Wong', 3);
+INSERT INTO Users VALUES(1008, 'Frank', 'frank@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Frank', 'Kim', 7);
+INSERT INTO Users VALUES(1009, 'Grace', 'grace@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Grace', 'Chen', 10);
+INSERT INTO Users VALUES(1010, 'Henry', 'henry@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Henry', 'Liu', 9);
+INSERT INTO Users VALUES(1011, 'Isabel', 'isabel@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Isabel', 'Zhang', 1);
+INSERT INTO Users VALUES(1012, 'Gavin', 'gavin@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Gavin', 'Gupta', 15);
+INSERT INTO Users VALUES(1013, 'Oliver', 'oliver@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Oliver', 'Mills', 11);
+INSERT INTO Users VALUES(1014, 'Penelope', 'penelope@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Penelope', 'Brown', 4);
+INSERT INTO Users VALUES(1015, 'Quentin', 'quentin@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Quentin', 'Jones', 12);
+INSERT INTO Users VALUES(1016, 'Rachel', 'rachel@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Rachel', 'Garcia', 6);
+INSERT INTO Users VALUES(1017, 'Samuel', 'samuel@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Samuel', 'Lee', 8);
+INSERT INTO Users VALUES(1018, 'Tara', 'tara@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Tara', 'Gupta', 2);
+INSERT INTO Users VALUES(1019, 'Uma', 'uma@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Uma', 'Patel', 14);
+INSERT INTO Users VALUES(1020, 'Violet', 'violet@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Violet', 'Singh', 3);
+INSERT INTO Users VALUES(1021, 'Wyatt', 'wyatt@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Wyatt', 'Chang', 13);
+INSERT INTO Users VALUES(1022, 'Jaxx', 'jboy@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Jackson', 'Bround', 7);
+INSERT INTO Users VALUES(1023, 'Yara', 'yara@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Yara', 'Kumar', 10);
+INSERT INTO Users VALUES(1024, 'Zachary', 'zachary@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Zachary', 'Nguyen', 1);
+INSERT INTO Users VALUES(1025, 'Alice', 'alice@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Alice', 'Ramirez', 11);
+INSERT INTO Users VALUES(1026, 'Brendan', 'brendan@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Brendan', 'Hernandez', 6);
+INSERT INTO Users VALUES(1027, 'Cynthia', 'cynthia@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Cynthia', 'Kim', 5);
+INSERT INTO Users VALUES(1028, 'Dexter', 'dexter@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Dexter', 'Patel', 14);
+INSERT INTO Users VALUES(1029, 'Eloise', 'eloise@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Eloise', 'Shah', 12);
+INSERT INTO Users VALUES(1030, 'Fiona', 'fiona@gmail.com', '$5$rounds=535000$/ckdaMK/e1Ovw3ds$WvQpZPNivK.VU8WLSI31JMftDZMfufKGQ1YMGcZiaz2', 'Fiona', 'Liu', 8);
+
 
 /*Deck fake data*/
 INSERT INTO Decks VALUES(11,'Math',TRUE);
 INSERT INTO Decks VALUES(12,'Hebrew',FALSE);
 INSERT INTO Decks VALUES(13,'German',TRUE);
 INSERT INTO Decks VALUES(14,'More Math',FALSE);
+INSERT INTO Decks VALUES(15, 'Biology', TRUE);
+INSERT INTO Decks VALUES(16, 'Spanish', FALSE);
+INSERT INTO Decks VALUES(17, 'Chemistry', TRUE);
+INSERT INTO Decks VALUES(18, 'World History', FALSE);
+INSERT INTO Decks VALUES(19, 'Literature', TRUE);
+INSERT INTO Decks VALUES(20, 'Geometry', TRUE);
+INSERT INTO Decks VALUES(21, 'French', FALSE);
+INSERT INTO Decks VALUES(22, 'Physical Ed', FALSE);
+INSERT INTO Decks VALUES(23, 'Art History', TRUE);
+INSERT INTO Decks VALUES(24, 'Geography', TRUE);
+INSERT INTO Decks VALUES(25, 'Music Theory', FALSE);
+INSERT INTO Decks VALUES(26, 'Computer Sci', TRUE);
+INSERT INTO Decks VALUES(27, 'Psychology', FALSE);
+INSERT INTO Decks VALUES(28, 'Environ. Sci', TRUE);
+INSERT INTO Decks VALUES(29, 'Political Sci', FALSE);
+INSERT INTO Decks VALUES(30, 'Astronomy', TRUE);
+INSERT INTO Decks VALUES(31, 'Economics', FALSE);
+INSERT INTO Decks VALUES(32, 'Theater', TRUE);
+INSERT INTO Decks VALUES(33, 'Communications', FALSE);
+INSERT INTO Decks VALUES(34, 'Philosophy', TRUE);
 
 /*Deck permissions fake data*/
 INSERT INTO DeckRights VALUES(1001, 11, 'User');
 INSERT INTO DeckRights VALUES(1002, 11, 'Owner');
+INSERT INTO DeckRights VALUES(1003, 11, 'Editor');
+INSERT INTO DeckRights VALUES(1004, 11, 'User');
 INSERT INTO DeckRights VALUES(1001, 12, 'Owner');
+INSERT INTO DeckRights VALUES(1005, 12, 'User');
+INSERT INTO DeckRights VALUES(1006, 12, 'User');
+INSERT INTO DeckRights VALUES(1007, 12, 'User');
 INSERT INTO DeckRights VALUES(1001, 13, 'Editor');
+INSERT INTO DeckRights VALUES(1008, 13, 'User');
+INSERT INTO DeckRights VALUES(1009, 13, 'User');
+INSERT INTO DeckRights VALUES(1010, 13, 'Owner');
 INSERT INTO DeckRights VALUES(1001, 14, 'User');
+INSERT INTO DeckRights VALUES(1011, 14, 'Owner');
+INSERT INTO DeckRights VALUES(1012, 14, 'User');
+INSERT INTO DeckRights VALUES(1013, 14, 'Editor');
+INSERT INTO DeckRights VALUES(1014, 15, 'Editor');
+INSERT INTO DeckRights VALUES(1015, 15, 'Owner');
+INSERT INTO DeckRights VALUES(1016, 15, 'User');
+INSERT INTO DeckRights VALUES(1017, 15, 'User');
+INSERT INTO DeckRights VALUES(1018, 16, 'Owner');
+INSERT INTO DeckRights VALUES(1019, 16, 'User');
+INSERT INTO DeckRights VALUES(1020, 16, 'Editor');
+INSERT INTO DeckRights VALUES(1001, 16, 'User');
+INSERT INTO DeckRights VALUES(1021, 17, 'User');
+INSERT INTO DeckRights VALUES(1022, 17, 'Owner');
+INSERT INTO DeckRights VALUES(1023, 17, 'Editor');
+INSERT INTO DeckRights VALUES(1024, 17, 'User');
+INSERT INTO DeckRights VALUES(1025, 18, 'User');
+INSERT INTO DeckRights VALUES(1026, 18, 'Owner');
+INSERT INTO DeckRights VALUES(1027, 18, 'User');
+INSERT INTO DeckRights VALUES(1001, 18, 'Editor');
+INSERT INTO DeckRights VALUES(1028, 19, 'Editor');
+INSERT INTO DeckRights VALUES(1029, 19, 'User');
+INSERT INTO DeckRights VALUES(1030, 19, 'User');
+INSERT INTO DeckRights VALUES(1011, 19, 'Owner');
+INSERT INTO DeckRights VALUES(1001, 20, 'Owner');
+INSERT INTO DeckRights VALUES(1022, 20, 'User');
+INSERT INTO DeckRights VALUES(1023, 20, 'Editor');
+INSERT INTO DeckRights VALUES(1024, 20, 'User');
+INSERT INTO DeckRights VALUES(1015, 21, 'User');
+INSERT INTO DeckRights VALUES(1016, 21, 'Editor');
+INSERT INTO DeckRights VALUES(1017, 21, 'User');
+INSERT INTO DeckRights VALUES(1018, 21, 'Owner');
+
 
 /*Card fake data*/
 INSERT INTO Cards VALUES(10001, 11, '2+2=','4');
@@ -73,6 +163,10 @@ INSERT INTO Cards VALUES(54, 12, 'תן', 'give');
 INSERT INTO Cards VALUES(55, 12, 'קח', 'take');
 INSERT INTO Cards VALUES(56, 12, 'טייל', 'travel');
 
+
+
+
 /*Friends fake data*/
 INSERT INTO Friends VALUES(1001, 1002);
+
 
